50e4b0a950feb2953471652388b8acf791c8827b
ephremdoyo <ephremdoyo@gmail.com>
2024-10-04 02:15:02 +0300

    Create .env file and implement user registration logic

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router=express.Router()
-
 const{register,checkuser,login}=require('../Controller/userController')
+
+// user register 
 router.post("/register",register)
+
+
   router.post("/login",login)
   router.get("/check",checkuser)
 
