a91a55c328b9cd99e811eb36a227ae5b51115c09
DevAbelo <abeldebalke12@gmail.com>
2024-10-05 00:21:33 +0300

    Update answerController.js

--- Controller/answerController.js
+++ Controller/answerController.js
@@ -5,22 +5,9 @@ const dbConnection = require("../db/dbconfig");
 async function getAllQuestions(req, res) {
   try {
     // Fetch all questions from the database
-    const [questions] = await dbConnection.query("SELECT * FROM questions");
+const [questions]= await dbConnection.query("select questions.id,questions.questionid,questions.userid,questions.title,questions.description,users.username from questions INNER JOIN users where questions.userid = users.userid")
+    return res.status(StatusCodes.OK).json({msg:"All question sent",questions})
 
-    // Check if there are no questions
-    if (questions.length === 0) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: "No questions found.",
-        total: 0,
-        questions: [],
-      });
-    }
-
-    // Respond with all questions and metadata
-    return res.status(StatusCodes.OK).json({
-      total: questions.length,
-      questions: questions,
-    });
   } catch (error) {
     console.error("Database query error:", error.message); // Log the error for debugging
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
