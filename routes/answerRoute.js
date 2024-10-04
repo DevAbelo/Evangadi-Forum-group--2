df1fbc6131fa42d37b4775607e93baff1faa9110
BEKMAL <bekalumsc@gmail.com>
2024-10-05 02:56:56 +0300

    auth middle wear added and route modified

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router=express.Router()
 
-const{ getAllQuestions,getAnswer}=require('../Controller/answerController')
-router.post("/answer", getAllQuestions);
-router.get("/answer/:question_id",getAnswer)
-  module.exports=router
+const{postAnswer,getAnswer}=require('../Controller/answerController')
+router.post("/answer",postAnswer)
+router.post("/answer/:question_id",getAnswer)  //better with post even if we try to get 
+module.exports=router                           //answer for specifice question
+
