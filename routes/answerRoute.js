167d141f4516a3e739de66ac5380b399af4e1527
BEKMAL <bekalumsc@gmail.com>
2024-10-05 03:31:39 +0300

    updated resolving conflict

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router=express.Router()
 
-const{postAnswer,getAnswer}=require('../Controller/answerController')
-router.post("/answer",postAnswer)
-router.post("/answer/:question_id",getAnswer)  //better with post even if we try to get 
-module.exports=router                           //answer for specifice question
-
+const{ getAllQuestions,getAnswer}=require('../Controller/answerController')
+router.post("/answer", getAllQuestions);
+router.get("/answer/:question_id",getAnswer)
+  module.exports=router
