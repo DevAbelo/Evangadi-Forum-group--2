5f3beec06bf4a2fe9a26fae35a0708e1082d6019
Bekalu Malede <102759163+BEKMAL@users.noreply.github.com>
2024-10-09 06:30:39 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 // import { useState } from "react";
 import { useContext, useEffect, useState } from "react";
 import Routing from "./Routing";
-import SignUp from "./Components/SignUp/SignUp"
+import { useNavigate } from "react-router-dom";
 function App() {
  const {user,setUser}=useContext(AppState)
-  // const [logedin,setlogedin]=useState("SIGNIN") //on context
-  localStorage.setItem("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJla2FsdSIsInVzZXJpZCI6MiwiaWF0IjoxNzI4NDQzMTU1LCJleHAiOjE3Mjg1Mjk1NTV9.bUm7KUrHkzmpMm9E_OJ_B29SvfCuH4rzOJiGetCxS7I")
   const token=localStorage.getItem('token')
   const navigate =useNavigate()
   async function checkuser() {
@@ -31,7 +29,6 @@ useEffect(()=>{
   return (
     <>
       <Routing />
-      {/* <SignUp/> */}
     </>
   );
 }
